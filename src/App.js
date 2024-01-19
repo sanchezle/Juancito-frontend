9a9a32e2d021ce9176be442b9ee70bd6a2fb7032
sanchezle <sanchezle21@gmail.com>
2024-01-19 21:00:42 +0100

    url backend changeed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function ChatApp() {
 
     const sendMessage = async () => {
         try {
-            const response = await axios.post('http://localhost:5000/juancito', { message: inputText });
+            const response = await axios.post(' https://juancito-f43f3b2f8207.herokuapp.com/juancito', { message: inputText });
             setMessages([...messages, { user: 'You', text: inputText }, { user: 'Bot', text: response.data.response }]);
         } catch (error) {
             console.error('Error:', error);
